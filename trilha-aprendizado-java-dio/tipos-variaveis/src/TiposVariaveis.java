1fe6cb9255da9a08ed14abb1717ba0b54e7e234d
RafaelaDiniz <rafaela2106@hotmail.com>
2022-11-02 20:19:51 -0300

    atualização

---
+++ trilha-aprendizado-java-dio/tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,22 @@
+public class TiposVariaveis {
+    
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+    
+        
+short numeroCurto = 1;
+int numeroNormal = numeroCurto;
+short numeroCurto = (short) numeroNormal;
+
+int numero = 5;
+
+ numero = 10;
+
+System.out.print(numero);
+final double VALOR_DE_PI = 3.14;
+
+    }
+}
