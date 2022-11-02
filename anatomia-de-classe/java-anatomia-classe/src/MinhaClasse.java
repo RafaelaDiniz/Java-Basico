813cc6fca1a265cbcdc892fd113152c6053eb985
RafaelaDiniz <rafaela2106@hotmail.com>
2022-11-02 18:28:15 -0300

    atualização

--- anatomia-de-classe/java-anatomia-classe/src/MinhaClasse.java
+++ anatomia-de-classe/java-anatomia-classe/src/MinhaClasse.java
@@ -1,5 +1,13 @@
 public class MinhaClasse {
     public static void main(String[] args) {
-        System.out.print("Olá Dio, sejam bem vindos");
+        String primeiroNome = "Rafaela";
+        String segundoNome = "Diniz";
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
     }
-}
+    public static String nomeCompleto (String primeiroNome, String segundoNome){ 
+        return "Resultado do método" + primeiroNome.concat("").concat(segundoNome);
+    }
+    }
+
