404474b2909aaf866475ad9f49a5d84e2da64208
RafaelaDiniz <rafaela2106@hotmail.com>
2022-11-02 22:08:11 -0300

    atualização

--- trilha-aprendizado-java-dio/operadores/src/Operadores.java
+++ trilha-aprendizado-java-dio/operadores/src/Operadores.java
@@ -1,5 +1,5 @@
 public class Operadores {
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
 String nomeCompleto = "LINGUAGEM" + "JAVA";
         System.out.println(nomeCompleto);
 
@@ -36,14 +36,24 @@ int a;
 int b;
 
 a = 5;
-b = 6;
-                      if(a==b) {
-                    } else {
-                    }
-                                                            
+b = 7;
+
+                    String resultado = a == b ? "verdadeiro" : "falso";
+                    System.out.println(resultado);
+                  
+                    int numero1 = 1;
+                    int numero2 = 2;
+                    boolean simNao = numero1 == numero2;
+                    if(numero1 < numero2){ System.out.println("a nossa condição é verdadeira");
+                }
+                    System.out.println("numeroUm é igual a numeroDois?" + simNao );
+                    simNao = numero1 > numero2;
+                    System.out.println("numeroUm é maior que numeroDois?" + simNao);
+
+
+                }}
 
 
 
+    
 
-    }
-}
