25b67725db4d8812e29d3137df54ddcc3d17ca7f
RafaelaDiniz <rafaela2106@hotmail.com>
2022-11-02 21:49:05 -0300

    atual

--- trilha-aprendizado-java-dio/operadores/src/Operadores.java
+++ trilha-aprendizado-java-dio/operadores/src/Operadores.java
@@ -2,5 +2,48 @@ public class Operadores {
     public static void main(String[] args)  {
 String nomeCompleto = "LINGUAGEM" + "JAVA";
         System.out.println(nomeCompleto);
+
+        String concatenacao ="?"; 
+
+concatenacao = 1+1+1+"1";
+System.out.println(concatenacao);
+
+concatenacao = 1+"1"+1+1;
+System.out.println(concatenacao);
+
+concatenacao = 1+"1"+1+"1";
+System.out.println(concatenacao);
+
+concatenacao = "1"+1+1+1;
+System.out.println(concatenacao);
+
+concatenacao = "1"+(1+1+1);
+System.out.println(concatenacao);
+
+
+int numero = 5;
+//x repeticao
+//numero = numero +1;
+
+System.out.println(++ numero);
+System.out.println(numero);
+
+boolean variavel = true;
+                               variavel = !variavel;
+                               System.out.println(!variavel);
+
+int a;
+int b;
+
+a = 5;
+b = 6;
+                      if(a==b) {
+                    } else {
+                    }
+                                                            
+
+
+
+
     }
 }
