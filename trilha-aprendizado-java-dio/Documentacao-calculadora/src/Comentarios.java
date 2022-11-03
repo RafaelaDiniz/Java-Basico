90331cfd72e65cc8156d4c0c8d58b7f8a3fc2289
RafaelaDiniz <rafaela2106@hotmail.com>
2022-11-03 00:47:05 -0300

    up

---
+++ trilha-aprendizado-java-dio/Documentacao-calculadora/src/Comentarios.java
@@ -0,0 +1,22 @@
+public class Comentarios {
+
+public static void main(String[] args) {
+   // todo document why this method is empty
+ 
+    //eu sou um comentario em uma linha
+
+    /* Olá,
+ * Eu sou um comentario
+ * que posso ser mais detalhadod
+ * quando necessário
+ */
+
+ /** 
+ * Estas duas estrelinhas acima
+ * é para identificar que você
+ * pretende elaborar um comentário
+ * a nível de documentação.
+ * Que incrível !!!
+ */
+ 
+}
