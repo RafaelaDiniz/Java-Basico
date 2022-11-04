aeccc62026932101676440d7c0458040c47b4eea
RafaelaDiniz <rafaela2106@hotmail.com>
2022-11-04 18:03:14 -0300

    exercicio

---
+++ trilha-aprendizado-java-dio/Logica-Condicional-Controle-de-fluxos-JAVA/a.operadores-relacionais/OperadoresRelacionais/README.md
@@ -0,0 +1,11 @@
+## Exercício de projeto da trilha de aprendizado
+ 
+ Exercitando o aprendizado da  aula **Operadores Relacionais** do curso **Lógica Condicional e Controles de Fluxo em Java** da *Digital Innovation One - DIO* **Bootcamp Potência Tech powered by iFood** - 
+ Java Beginners
+#
+#
+
+#
+
+[![Imagem logo java](./../Imagens/logo_java.png)](https://www.java.com/pt-BR)
+
---
+++ trilha-aprendizado-java-dio/Logica-Condicional-Controle-de-fluxos-JAVA/a.operadores-relacionais/OperadoresRelacionais/src/OperadoresRelacionais.java
@@ -0,0 +1,62 @@
+import javax.lang.model.util.ElementScanner14;
+
+public class OperadoresRelacionais {
+    public static void main(String[] args)  {
+        
+        // realize comparações 
+     
+        int i1 = 10;
+        int i2 = 20;
+        
+        if (i1 >= i2);
+       System.out.println("Perdeu i1 é maior ou igual a i2");
+        if (i1 == i2);
+       System.out.println("São valores iguais i1 e i2");
+       if (i2 > i1);
+       System.out.println("Ganhei i1 é maior qe i2");
+
+
+            float f1 = 4.5f;
+            float f2 = 3.5f;
+            if (f2 <= f1);
+            System.out.println("Perdeu f2 é menor ou igual a f1");
+            if (f1 == f2);
+            System.out.println("São valores iguais f1 e f2 são iguais");
+            if (f1 > f2);
+            System.out.println("Ganhei f1 é maior que f2");
+            if (f1 != f2);
+            System.out.println("ninguem ganhou f1 e f2 são diferentes");
+                  
+           char c1 = 'x';
+                  char c2 = 'y';
+                  if (c1 <= c2 );
+            System.out.println("Perdeu c2 é menor que c1");
+            if (c1 == c2);
+            System.out.println("São valores iguais c1 e c2");
+            if (c1 != c2);
+            System.out.println("Ganhei c1 é diferente de c2");
+
+                      String s1 = "Fulano";
+                      String s2 = "Fulano";
+                      
+                      if (s1 != s2 );
+            System.out.println("Fulano é diferente");
+             if (s1 == s2);
+            System.out.println("Fulano é igual");
+                             
+                      boolean b1 = true;
+                             boolean b2 = false;
+    
+     
+ 
+
+ 
+
+
+
+
+
+
+
+    }
+}
